92c60ed2a53ef4446922ccc0160ac70a1b6f1543
Ahaubro <alex_haubro@hotmail.com>
2022-08-13 12:27:07 +0200

    Added clear function to clear inputs in my Q&A

--- Server/ssr.js
+++ Server/ssr.js
@@ -32,35 +32,31 @@ ssr.directory = '../../Client/Public/Pages';
 const template_page_template = ssr.loadFile('/template_page.html')
 const template_page = ssr.replace(template_page_template, { navbar, footer })
 
-const home_style = `<link rel="stylesheet" type="text/css" href="./Style/home_style.css">`
-const about_style = `<link rel="stylesheet" type="text/css" href="./Style/about_style.css">`
-const cv_style = `<link rel="stylesheet" type="text/css" href="./Style/cv_style.css">`
-const contact_style = `<link rel="stylesheet" type="text/css" href="./Style/contact_style.css">`
 
 
 const home_page = ssr.replace(template_page, {
     title: 'Alex page',
-    styles: home_style,
+    styles: '',
     content: '/index.html'
 });
 
 
 const cv_page = ssr.replace(template_page, {
     title: 'Alex cv',
-    styles: cv_style,
+    styles: '',
     content: '/cv_page.html'
 });
 
 
 const about_page = ssr.replace(template_page, {
     title: 'Who is Alex?',
-    styles: about_style,
+    styles: '',
     content: '/about_page.html'
 });
 
 const contact_page = ssr.replace(template_page, {
     title: 'Contact me today!',
-    styles: contact_style,
+    styles: '',
     content: '/contact_page.html'
 });
 
